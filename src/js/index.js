06fe50d69885dc9a2fe00785af73e4f55349133a
Your Name <you@example.com>
2017-07-29 17:54:18 +0800

    提交项目

--- src/js/index.js
+++ src/js/index.js
@@ -11,19 +11,79 @@ $(function () {
 
         $(".big_container_img_mengban").eq($(this).index()).show();
         $(".hengxian").eq($(this).index()).show()
-    },function () {
+    }, function () {
         $(".big_container_img_mengban").eq($(this).index()).hide();
         $(".hengxian").eq($(this).index()).hide()
 
     });
+
     //TING图片部分
-    $(".img_di_fu").hover(function () {
-            $(".img_di_fu").eq()
+    $(".Ting_img_d1").hover(function () {
+        $(".Ting_img_d1 img").css({
+            transition: ' all 2s',
+            transform: 'scale(1.1)'
+        });
+        $(".Ting_img_mengban1").show()
+    }, function () {
+        $(".Ting_img_d1 img").css({
+            transform: 'scale(1)'
+        });
+        $(".Ting_img_mengban1").hide()
+    });
 
+    $(".Ting_img_d2").hover(function () {
+        $(".Ting_img_d2 img").css({
+            transition: ' all 2s',
+            transform: 'scale(1.1)'
+        });
+        $(".Ting_img_mengban2").show()
+    }, function () {
+        $(".Ting_img_d2 img").css({
+            transform: 'scale(1)'
+        });
+        $(".Ting_img_mengban2").hide()
+    });
+
+    $(".Ting_img_d3").hover(function () {
+        $(".Ting_img_d3 img").css({
+            transition: ' all 2s',
+            transform: 'scale(1.1)'
+        });
+        $(".Ting_img_mengban3").show()
+    }, function () {
+        $(".Ting_img_d3 img").css({
+            transform: 'scale(1)'
+        });
+        $(".Ting_img_mengban3").hide()
+    });
+
+    //底部
+    $(".foot_wechat").hover(function () {
+
+        $(".pianke-code").show();
     },function () {
+        $(".pianke-code").hide();
 
+    });
+    //返回顶部
+    $(".return").click(function () {
+        $(window.document).scrollTop(0);
+    });
+    //导航栏响应事件
+
+    var up = 0;
+    $(window.document).scroll(function () {
+        var scrollTop = $(document).scrollTop();
+        if(scrollTop > up){
+            if(scrollTop >= 90){
+              $(".container").slideUp()
+            }
+        }else {
+            $(".container").slideDown(1)
+
+        }
+        up = scrollTop;
     })
-
 
 
 });
